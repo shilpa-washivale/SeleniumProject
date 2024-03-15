0879f95d28fda09a00d06ac8579941d77f278930
Shilpa <shilpaw2210@gmail.com>
2024-03-15 16:31:39 +0530

    First commit

---
+++ src/Intro.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+ 
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\WORD\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+
+	}
+
+}
